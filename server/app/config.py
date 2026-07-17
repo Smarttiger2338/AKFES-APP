3a897016187512f14e572d6e255f4bdd31525a75
Chiwon <71111212+Smarttiger2338@users.noreply.github.com>
2026-07-18 03:56:25 +0900

    Allow AKFES admin development origin

--- server/app/config.py
+++ server/app/config.py
@@ -96,6 +96,8 @@ def get_settings() -> Settings:
             (
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
+                "http://localhost:5174",
+                "http://127.0.0.1:5174",
                 "http://localhost:1420",
                 "http://127.0.0.1:1420",
                 "http://tauri.localhost",
